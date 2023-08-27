692e51d7de38df593d6ddb60090b8deebadbff8c
21f1000899 <143321995+21f1000899@users.noreply.github.com>
2023-08-27 15:47:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,4 @@ st.write("""
 a= st.number_input("NUMBER_1", min_value=0.0,max_value=2000000000.0)
 b= st.number_input("NUMBER_2", min_value=0.0,max_value=2000000000.0)
 c= st.number_input("NUMBER_3", min_value=0.0,max_value=2000000000.0)
+st.write(a)
