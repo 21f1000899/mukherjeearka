1780e5626042c8f0e828424daf22029121f158f4
21f1000899 <143321995+21f1000899@users.noreply.github.com>
2023-08-27 15:50:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ st.write("""
 a= st.number_input("NUMBER_1", min_value=0.0,max_value=2000000000.0)
 b= st.number_input("NUMBER_2", min_value=0.0,max_value=2000000000.0)
 c= st.number_input("NUMBER_3", min_value=0.0,max_value=2000000000.0)
-max=np.max(a,b,c)
+max=np.max([a,b,c])
 st.write("The Largest Number is"+max)
