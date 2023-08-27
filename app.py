8b5f076e08009898e2f76b8508ef4699559e4abd
21f1000899 <143321995+21f1000899@users.noreply.github.com>
2023-08-27 15:49:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ import pandas as pd
 import numpy as np
 
 st.write("""
-#  Hello world
+#Largest Number
 """)
 a= st.number_input("NUMBER_1", min_value=0.0,max_value=2000000000.0)
 b= st.number_input("NUMBER_2", min_value=0.0,max_value=2000000000.0)
 c= st.number_input("NUMBER_3", min_value=0.0,max_value=2000000000.0)
-st.write(a)
+max=np.max(a,b,c)
+st.write("The Largest Number is"+max)
