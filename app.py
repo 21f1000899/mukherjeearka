c1c43367daa91589fd58b734ced5040dad3d8786
21f1000899 <143321995+21f1000899@users.noreply.github.com>
2023-08-27 15:46:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ import numpy as np
 st.write("""
 #  Hello world
 """)
-a= st.number_input("NUMBER", min_value=0.0,max_value=2000000000.0)
-b= st.number_input("NUMBER", min_value=0.0,max_value=2000000000.0)
-c= st.number_input("NUMBER", min_value=0.0,max_value=2000000000.0)
+a= st.number_input("NUMBER_1", min_value=0.0,max_value=2000000000.0)
+b= st.number_input("NUMBER_2", min_value=0.0,max_value=2000000000.0)
+c= st.number_input("NUMBER_3", min_value=0.0,max_value=2000000000.0)
